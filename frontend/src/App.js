4bb76bf9664e16c4c95815de6b5c75b6bb0c98c9
Antonio Gallardo Girela <gallardogirela@gmail.com>
2022-12-28 13:04:18 +0100

    created Home and Chat pages and added tailwindcss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './Pages/Home.js';
+import Chat from './Pages/Chat';
 
 function App() {
-  return <div className='App'>Hello World!</div>;
+  return (
+    <div className='App'>
+      <Route path='/' component={Home} exact></Route>
+      <Route path='/chats' component={Chat}></Route>
+    </div>
+  );
 }
 
 export default App;
